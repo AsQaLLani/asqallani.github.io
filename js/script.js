83c8e35a55fce97b41b9e0e1beecf1fcf766736f
Ibnu Asqallani <qallanigamincorp@gmail.com>
2019-10-07 08:56:11 +0700

    4.3 added /game

--- js/script.js
+++ js/script.js
@@ -4,30 +4,40 @@ $('.page-scroll').on('click',function(e){
 
 	var elemenTujuan=$(tujuan);
 
-	$('body').scrollTop(elemenTujuan.offset().top);
-	
+	$('body').animate({
+	scrollTop:elemenTujuan.offset().top
+	},{easing:'swing'});
 });
 
+$('#tombol').click(function(){
+	console.log($("body").scrollTop(300));
+	$('body').animate({height:250,width:400},{easing:'swing'});
+
+});
 
 //password Birthday
 function passwordBdButton(){
 	var passbdasli="anjaymabar";
 	var inputPassBd=document.getElementById('BdInput').value;
 	if(inputPassBd==passbdasli){
-		location.href="https://asqallani.github.io/bday";
+		alert("Correct");
+		window.open("https://asqallani.github.io/bday","_blank");
 	}
 	else{
-		alert("Salah Njing");	
+		alert("Salah");
 }}
 
 
 //showhide Birthday password
 $('#BdForm').hide();
-$('#birthday').on('click',function(){
+
+$('#birthday').each(function(){
+	$(this).click(function(){
+	var boolBirthday=true;
 	$('#BdForm').toggle();
+	$('#BdForm').animate({height:250,width:300},{easing:'swing'});
 });
-
-
+});
 
 
 //last
